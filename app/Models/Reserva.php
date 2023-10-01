bae5f3b99b14904a061ae4b9beed576a2f7658b3
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2023-10-01 19:22:55 -0300

    update Reserva model

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Reserva extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $table = 'Reserva';
+    protected $primaryKey = 'id_reserva';
+    protected $fillable = [
+        'data_inicio',
+        'data_previsao',
+        'nota_fiscal_pag',
+        'id_multa',
+        'placa_veiculo',
+        'id_cliente'
+    ];
 }
